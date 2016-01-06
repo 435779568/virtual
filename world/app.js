9111d95497403bbb14a4fba8fb9ce50a5458c553
YangChao <chao.yang@mozzos.com>
2016-01-06 16:48:32 +0800

    introduce

---
+++ world/app.js
@@ -0,0 +1,6 @@
+//This is a test of d3 project, we want to create a 3d virtual world, through javascript to create the whole project.
+/* 
+ 将使用angular.js处理前端 node.js架载服务器
+ requirejs组织文件 d3.js实现核心 辅之以jq 
+ 整个项目必须清洁,简单,追求并发效率以及用户体验
+ 
